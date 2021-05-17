b4f41d7ff28ba859bd1ab893781d766a8f73dc2e
Jonghun Lee <jonghunjhl@gmail.com>
2021-05-16 23:31:11 -0600

    first commit

---
+++ trtopr.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const app = express();
+const port = 3001;
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
+
+const crawl = async ({ url }) => {
+	let obj = {};
+
+	const response = await fetch(url);
+	const dataFromAPI = await response.json();
+
+	// read the current file and save to obj
+	let data = getJsonData();
+	obj = JSON.stringify(data);
+
+	// append dataFromAPI to currentData
+	obj = {
+		...data,
+		[`${Object.keys(data).length + 1}`]: dataFromAPI,
+	};
+
+	let finalData = JSON.stringify(obj);
+
+	// write updated data to data.json
+	fs.writeFile("data.json", finalData, (err) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`JSON data is saved id ${Object.keys(data).length + 1}`);
+	});
+};
+
+const getJsonData = () => {
+	const data = fs.readFileSync("data.json");
+	const result = JSON.parse(data);
+
+	return result;
+};
+
+// execute the function every hour
+setInterval(function () {
+	crawl({
+		url: "https://uw9yf1u6qf.execute-api.ca-central-1.amazonaws.com/prod/live-counter",
+	});
+}, 3600000);
+
+app.get("/data", (req, res) => {
+	const data = getJsonData();
+	res.send(data);
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
