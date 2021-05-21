69d58853ccefcdac7d301a81c04a411f9d9a48e2
Jonghun Lee <jonghunjhl@gmail.com>
2021-05-21 11:20:31 -0600

    ssl certificate testing

--- trtopr.js
+++ trtopr.js
@@ -6,7 +6,7 @@ const https = require('https');
 const useragent = require('express-useragent');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 81;
 
 app.use(express.static('public'));
 
